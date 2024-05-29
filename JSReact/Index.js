c7c70a142ead5cc9b26b3f2c3e6ca4a9f7e0ca10
JVLTESAA <valentin070522mail.com>
2024-05-28 23:57:03 -0600

    Se agrega la carpeta con los avances de la práctica numero 2

---
+++ JSReact/Index.js
@@ -0,0 +1,27 @@
+console.log('Hola consola')
+
+//Funciones 
+
+//Tradicional
+
+function suma(a,b){
+    return a+b
+}
+
+//Fat Arrow  funtion
+//onst resta= (a,b)=>{ return a-b }
+
+//Fat Arrow simplificada
+const resta= (a,b)=> a-b
+
+console.log(suma(1,10))
+console.log(resta(100,10))
+
+// OBJETOS
+
+const usuario={
+    nombre:'ana', 
+    apellidos: 'Guerra',
+    edad: 30,
+    direccion: 'C.Ocampo'
+}
